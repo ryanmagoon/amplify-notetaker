6ffca8f9033e995c5f50a76c779a82197e13f73d
Ryan Magoon <mr.magoon5@gmail.com>
2019-08-05 00:51:41 -0400

    auth configured and working

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import Amplify from 'aws-amplify'
 import { withAuthenticator } from 'aws-amplify-react'
+import awsExports from './aws-exports'
+
+Amplify.configure(awsExports)
 
 const App: React.FC = () => {
   return <div>App</div>
